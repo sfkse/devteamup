867fa2e6b9f0fe0f8bbc1610cb52e78d3d331bc4
sfkse <sfkse5591@gmail.com>
2024-04-02 22:44:57 +0200

    Update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PricePage from "./pages/PricePage";
-import AppWrapper from "./components/AppWrapper.JSX";
+import AppWrapper from "./components/AppWrapper";
 
 function App() {
   return (
